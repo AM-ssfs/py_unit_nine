5fb2e5d022ee5c626848ebe40b2ad25824f91464
AM-ssfs <menscad@ssfs.org>
2021-12-09 14:30:27 -0500

    dog class

--- deck.py
+++ deck.py
@@ -1,28 +1,14 @@
-from card import Card
+import card
 
 
 class Deck:
     def __init__(self):
         self.cards = []
-
-    def make_deck(self):
-        Deck.__init__(self)
-        for x in range(2, 15):
-            self.cards.append(Card("Hearts", x))
-        for x in range(2, 15):
-            self.cards.append(Card("Diamonds", x))
-        for x in range(2, 15):
-            self.cards.append(Card("Clubs", x))
-        for x in range(2, 15):
-            self.cards.append(Card("Spades", x))
-        return self.cards
+        ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+        suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
+        for suit in suits:
+            for rank in ranks:
+                self.cards.append(card.Card(suit, rank))
 
 
-def main():
-    deck = Deck.make_deck(Deck)
-    for x in range(52):
-        print(str(deck[x].rank) + " of " + deck[x].suit)
-
-
-if __name__ == '__main__':
-    main()
+Deck()
