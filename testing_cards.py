1968c854bd779c6ef88ea755a169d33ded0e1293
AM-ssfs <menscad@ssfs.org>
2021-12-14 09:23:39 -0500

    cards

--- testing_cards.py
+++ testing_cards.py
@@ -1,5 +1,14 @@
 import deck
+import card
 
 new_deck = deck.Deck()
 
 print(new_deck.cards[1].rank + " of " + new_deck.cards[1].suit)
+print(new_deck.cards[1])
+
+newcard = card.Card("Hearts", "4")
+newercard = card.Card("Hearts", "8")
+
+print(newcard)
+if newercard > newcard:
+    print("eee")
